7e68264449acfb9408a678fd7554a891b676b414
Glo18 <kiogoraspecial@gmail.com>
2021-03-07 18:41:10 +0300

    create tests

---
+++ src/test/java/dao/Sql2oDepartmentNewsDaoTest.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.junit.Assert.*;
+
+public class Sql2oDepartmentNewsDaoTest {
+
+    public void setUp() throws Exception {
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
---
+++ src/test/java/dao/Sql2oGeneralNewsDaoTest.java
@@ -0,0 +1,13 @@
+package dao;
+
+import junit.framework.TestCase;
+
+public class Sql2oGeneralNewsDaoTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
