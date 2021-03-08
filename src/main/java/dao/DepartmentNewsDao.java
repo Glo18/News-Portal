eab5f2027e11190fcc5656ffbf4ef16f80591ea0
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:49:12 +0300

    add methods in department news interface

--- src/main/java/dao/DepartmentNewsDao.java
+++ src/main/java/dao/DepartmentNewsDao.java
@@ -1,8 +1,16 @@
+
 package dao;
 
 import models.DepartmentNews;
 
+
+import java.util.List;
+
 public interface DepartmentNewsDao {
-    static void add(DepartmentsNews testDepartmentNews) {
-    }
+    void add(DepartmentNews departmentNews);
+
+    List<DepartmentNews> getAllDepartmentNews();
+
+    DepartmentNews getDepartmentNewsById(int id);
+
 }
