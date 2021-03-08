b065ce7cd763a528714c5b927654505fd29b9bcc
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:35:04 +0300

    Sql2o implemention of generalNews

--- src/test/java/dao/Sql2oGeneralNewsDaoTest.java
+++ src/test/java/dao/Sql2oGeneralNewsDaoTest.java
@@ -1,13 +1,49 @@
 package dao;
 
-import junit.framework.TestCase;
+import models.DepartmentNews;
+import models.Departments;
+import models.GeneralNews;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-public class Sql2oGeneralNewsDaoTest extends TestCase {
+import static org.junit.Assert.*;
 
+public class Sql2oGeneralNewsDaoTest {
+
+    private Connection conn;
+    private Sql2oGeneralNewsDao GeneralNewsDao;
+    private Sql2oDepartmentNewsDao DepartmentsNewsDao;
+
+    private static  Sql2o sql2o;
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        String connectionString = "jdbc:postgresql://localhost:5432/departments_test";
+        Sql2o sql2o = new Sql2o(connectionString, "moringa", "Nya2rango@");
+        GeneralNewsDao = new Sql2oGeneralNewsDao(sql2o);
+//        DepartmentNewsDao = new Sql2oDepartmentNewsDao(sql2o);
+        conn = sql2o.open();
+
     }
 
+    @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addingGeneralNewsSetsId() throws Exception {
+        GeneralNews testGeneralNews = setupNewGeneralNews();
+        int originalGeneralNewsId = testGeneralNews.getId();
+        GeneralNewsDao.add(testGeneralNews);
+        assertNotEquals(originalGeneralNewsId ,testGeneralNews.getId());
+    }
+
+
+    //helper
+    public GeneralNews setupNewGeneralNews(){
+        return new GeneralNews ("Psychology", "Mental wellness", "Gachoki");
     }
 }
