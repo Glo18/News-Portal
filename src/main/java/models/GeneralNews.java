38ed97ec13734d079ef74e7a601aa016e6b09226
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:16:05 +0300

    add tests,throw exceptions

--- src/main/java/models/GeneralNews.java
+++ src/main/java/models/GeneralNews.java
@@ -1,4 +1,64 @@
 package models;
 
+import java.util.Objects;
+
 public class GeneralNews {
+    private int id;
+    private String title;
+    private String news;
+    private String author;
+
+    public GeneralNews(String title, String news, String author) {
+        this.title = title;
+        this.news = news;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GeneralNews)) return false;
+        GeneralNews that = (GeneralNews) o;
+        return id == that.id &&
+                Objects.equals(title, that.title) &&
+                Objects.equals(news, that.news) &&
+                Objects.equals(author, that.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, news, author);
+    }
 }
--- src/test/java/models/GeneralNewsTest.java
+++ src/test/java/models/GeneralNewsTest.java
@@ -15,4 +15,24 @@ public class GeneralNewsTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void instantiatesGeneralNewsDetails() throws Exception{
+        GeneralNews testGeneralNews = setUpGeneralNews();
+        assertEquals("TeamWork", testGeneralNews.getTitle());
+        assertEquals("GoodWork", testGeneralNews.getNews());
+        assertEquals("Glory", testGeneralNews.getAuthor());
+    }
+
+    @Test
+    public void setId(){
+        GeneralNews testGeneralNews = setUpGeneralNews();
+        testGeneralNews.setId(4);
+        assertEquals(4,testGeneralNews.getId());
+    }
+
+    //    helper
+    public GeneralNews setUpGeneralNews(){
+        return new GeneralNews("TeamWork", "GoodWork", "Glory");
+    }
 }
