1e5dff8a35625ff4a6f0dbcc65370d19616b5292
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:06:25 +0300

    add a test for users

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,59 @@
 package models;
 
 public class Users {
+    private int id;
+    private String username;
+    private String userPosition;
+    private String userRole;
+    private int departmentId;
+
+    public Users(String username, String userPosition, String userRole, int departmentId) {
+        this.username = username;
+        this.userPosition = userPosition;
+        this.userRole = userRole;
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserPosition() {
+        return userPosition;
+    }
+
+    public void setUserPosition(String userPosition) {
+
+        this.userPosition = userPosition;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
 }
--- src/test/java/models/UsersTest.java
+++ src/test/java/models/UsersTest.java
@@ -1,13 +1,27 @@
 package models;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class UsersTest extends TestCase {
+public class UsersTest  {
 
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
     }
 
+    @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void instantiatesUsersDetails() throws Exception{
+        Users testUsers = setUpUsers();
+        assertEquals("Steve", testUsers.getUsername());
+        assertEquals("Accountant", testUsers.getUserPosition());
+        assertEquals("Accounting", testUsers.getUserRole());
+        assertEquals(0, testUsers.getDepartmentId());
+
     }
 }
