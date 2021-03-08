8f2e4981f21636274d74b629ecc44e0ceeb8a613
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:31:13 +0300

    add methods in user interface

--- src/main/java/dao/Sql2oDepartmentsDao.java
+++ src/main/java/dao/Sql2oDepartmentsDao.java
@@ -1,4 +1,8 @@
 package dao;
 
+import org.sql2o.Sql2o;
+
 public class Sql2oDepartmentsDao {
+    public Sql2oDepartmentsDao(Sql2o sql2o) {
+    }
 }
--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -1,4 +1,12 @@
 package dao;
 
+import models.Users;
+
+import java.util.List;
+
 public interface UsersDao {
+    void add(Users users);
+
+    List<Users> getAllUser();
+    Users getUserById(int id);
 }
