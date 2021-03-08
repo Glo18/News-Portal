103df495d6890a812216696352c0cadf4f738992
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:39:23 +0300

    Sql2o implementation of departmentsDao

--- src/main/java/dao/Sql2oDepartmentsDao.java
+++ src/main/java/dao/Sql2oDepartmentsDao.java
@@ -1,8 +1,85 @@
 package dao;
 
+import models.DepartmentNews;
+import models.Departments;
+import models.Users;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
-public class Sql2oDepartmentsDao {
+import java.util.List;
+
+public class Sql2oDepartmentsDao implements DepartmentsDao{
+    private final Sql2o sql2o;
+
     public Sql2oDepartmentsDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+
+    @Override
+    public void add(Departments departments) {
+        String sql ="INSERT INTO departments (departmentname, departmentdescription, noofemployees) VALUES (:departmentname, :departmentdescription, :noofemployees )";
+        try (Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(departments)
+                    .addParameter("departmentname", departments.getDepartmentName())
+                    .addParameter("departmentdescription",departments.getDepartmentDescription())
+                    .addParameter("noofemployees", departments.getNoOfEmployees())
+                    .executeUpdate()
+                    .getKey();
+            departments.setId(id);
+        }
+    }
+
+    @Override
+    public List<Departments> getAllDepartments() {
+        String sql = "SELECT * FROM departments";
+        try (Connection con = sql2o.open()){
+            return con.createQuery(sql, true)
+                    .executeAndFetch(Departments.class);
+        }catch (Sql2oException ex) {
+            System.out.println(ex);
+            return null;
+        }
+    }
+
+    @Override
+    public List<DepartmentNews> getDepartmentNewsById(int id) {
+        String sql = "SELECT * FROM news WHERE departmentId= :id";
+        try (Connection con = sql2o.open()) {
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetch(DepartmentNews.class);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+            return null;
+        }
+    }
+
+    @Override
+    public List<Users> getDepartmentUserById(int id) {
+        String sql = "SELECT * FROM users WHERE departmentId= :id";
+        try (Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetch(Users.class);
+        }catch (Sql2oException ex) {
+            System.out.println(ex);
+            return null;
+        }
+    }
+
+    @Override
+    public Departments getDepartmentsById(int id) {
+        String sql = "SELECT * FROM departments WHERE id = :id";
+        try (Connection con = sql2o.open()){
+            return  con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Departments.class);
+        }catch (Sql2oException ex) {
+            System.out.println(ex);
+            return null;
+        }
     }
 }
--- src/test/java/dao/Sql2oDepartmentsDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentsDaoTest.java
@@ -1,13 +1,47 @@
 package dao;
 
-import junit.framework.TestCase;
+import models.Departments;
+import models.Users;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-public class Sql2oDepartmentsDaoTest extends TestCase {
+import static org.junit.Assert.*;
 
+public class Sql2oDepartmentsDaoTest {
+
+    private Connection conn;
+    private Sql2oUsersDao UsersDao;
+    private Sql2oDepartmentsDao DepartmentsDao;
+
+    private static Sql2o sql2o;
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        String connectionString = "jdbc:postgresql://localhost:5432/departments_test";
+        Sql2o sql2o = new Sql2o(connectionString, "moringa", "Nya2rango@");
+        UsersDao = new Sql2oUsersDao(sql2o);
+        DepartmentsDao = new Sql2oDepartmentsDao(sql2o);
+        conn = sql2o.open();
     }
 
+    @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void addingDepartmentsSetsId() throws Exception {
+        Departments testDepartments = setupNewDepartments();
+        int originalDepartmentsId = testDepartments.getId();
+        DepartmentsDao.add(testDepartments);
+        assertNotEquals(originalDepartmentsId,testDepartments.getId());
+    }
+
+    //helper
+    public Departments setupNewDepartments(){
+        return new Departments ("Finance", "Accounts", 30);
+    }
+
 }
