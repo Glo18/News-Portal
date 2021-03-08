565c19d1ccadc26e3b510361de1031906a973ef3
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:01:14 +0300

    create tests

---
+++ src/test/java/models/UsersTest.java
@@ -0,0 +1,13 @@
+package models;
+
+import junit.framework.TestCase;
+
+public class UsersTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
