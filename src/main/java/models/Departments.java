0a27472566e4c9f34768d6a4efb40baa585dd447
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:19:48 +0300

    add tests,throw exceptions for departments

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,88 @@
 package models;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Departments {
+    private int id;
+    private String departmentName;
+    private String departmentDescription;
+    private int noOfEmployees;
+    private List<Users> departmentUsers;
+    private List<DepartmentNews> departmentNews;
+
+    public Departments(String departmentName, String departmentDescription, int noOfEmployees) {
+        this.departmentName = departmentName;
+        this.departmentDescription = departmentDescription;
+        this.noOfEmployees = noOfEmployees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentDescription() {
+        return departmentDescription;
+    }
+
+    public void setDepartmentDescription(String departmentDescription) {
+        this.departmentDescription = departmentDescription;
+    }
+
+    public int getNoOfEmployees() {
+        return noOfEmployees;
+    }
+
+    public void setNoOfEmployees(int noOfEmployees) {
+
+        this.noOfEmployees = noOfEmployees;
+    }
+
+    public List<Users> getDepartmentUsers() {
+
+        return departmentUsers;
+    }
+
+    public void setDepartmentUsers(List<Users> departmentUsers) {
+        this.departmentUsers = departmentUsers;
+    }
+
+    public List<DepartmentNews> getDepartmentNews() {
+        return departmentNews;
+    }
+
+    public void setDepartmentNews(List<DepartmentNews> departmentNews) {
+        this.departmentNews = departmentNews;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departments)) return false;
+        Departments that = (Departments) o;
+        return getId() == that.getId() &&
+                getNoOfEmployees() == that.getNoOfEmployees() &&
+                Objects.equals(getDepartmentName(), that.getDepartmentName()) &&
+                Objects.equals(getDepartmentDescription(), that.getDepartmentDescription()) &&
+                Objects.equals(getDepartmentUsers(), that.getDepartmentUsers()) &&
+                Objects.equals(getDepartmentNews(), that.getDepartmentNews());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getDepartmentName(), getDepartmentDescription(), getNoOfEmployees(), getDepartmentUsers(), getDepartmentNews());
+    }
+
 }
--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -1,13 +1,44 @@
 package models;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class DepartmentsTest extends TestCase {
+import static org.junit.Assert.*;
 
+public class DepartmentsTest {
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
     }
 
+    @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void instantiatesDepartmentsDetails() throws Exception{
+        Departments testDepartments = SetUpDepartments();
+        assertEquals("production", testDepartments.getDepartmentName());
+        assertEquals("valuation", testDepartments.getDepartmentDescription());
+        assertEquals(0, testDepartments.getNoOfEmployees());
+    }
+
+    @Test
+    public void  getDepartmentDescription() {
+        Departments testDepartments = SetUpDepartments();
+        assertEquals("valuation", testDepartments.getDepartmentDescription());
+    }
+
+    @Test
+    public void setId(){
+        Departments testDepartments = SetUpDepartments();
+        testDepartments.setId(4);
+        assertEquals(4,testDepartments.getId());
+    }
+
+    public Departments SetUpDepartments() {
+        return new Departments("production", "valuation", 0);
+
     }
 }
