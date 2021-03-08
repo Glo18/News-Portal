f013d6b1ee55a7571e23f641bae405e703ec1d57
Glo18 <kiogoraspecial@gmail.com>
2021-03-08 03:37:10 +0300

    add getters in Department interface

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import models.DepartmentNews;
+import models.Departments;
+import models.Users;
+
+import java.util.List;
+
 public interface DepartmentsDao {
+
+    void add(Departments department);
+
+    List<Departments> getAllDepartments();
+    List<DepartmentNews>getDepartmentNewsById( int id);
+    List<Users> getDepartmentUserById (int id);
+    Departments getDepartmentsById(int id);
+
 }
